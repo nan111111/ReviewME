f0ee5e74d34529f95909aad8ac3ed90c71de20ed
swJo <45043145+KrswJo@users.noreply.github.com>
2021-10-08 17:38:42 +0900

    Update CarExample.java
    내가 원하는 방향으로 짜봤습니다.

--- src/chap6/CarExample.java
+++ src/chap6/CarExample.java
@@ -2,52 +2,29 @@ package chap6;
 import java.util.*;
 public class CarExample {
 	public static void main(String[] args) {
+		selfNum();
+	}
+	
+	public static void selfNum(){
 		int a = 0;
-		for(int i = 1; i < 10000; i++){
-	        
-	     
-	        if(1 <= i && i < 10) {
-		        a = i + i;
-	        	System.out.println(a);
-	        }
-	        if (10 <= i && i < 100) {
-	        	int b = 0;
-	        	int c = 0;
-	        	b = i % 10;// 1ÀÇ ÀÚ¸®
-	        	c = i / 10;//10ÀÇ ÀÚ¸®
-	        	a = i + b + c;
-	        	System.out.println(a);
-	        }
-	        if (100 <= i && i < 1000) {
-	        	int b = 0;
-	        	int c = 0;
-	        	int d = 0;
-	        	b = i % 10;// 1ÀÇ ÀÚ¸®
-	        	d = i / 100;//100ÀÇ ÀÚ¸®
-	        	c = (i / 10) - d;//10ÀÇ ÀÚ¸®
-	        	a = i + b + c + d;
-	        	System.out.println(a);
-	        }
-	        if (1000 <= i && i < 10000) {
-	        	int b = 0;
-	        	int c = 0;
-	        	int d = 0;
-	        	int e = 0;
-	        	b = i % 10;// 1ÀÇ ÀÚ¸®
-	        	e = i / 1000;//1000ÀÇ ÀÚ¸®
-	        	d = (i / 100) - e;//100ÀÇ ÀÚ¸®
-	        	c = (i / 10) - d - e;//10ÀÇ ÀÚ¸®
-	        	a = i + b + c + d + e;
-	        	System.out.println(a);
-	        }
-	        if(i == 10000) {
-	        	a = 10000 + 1;
-	        }
-	        
-	        
+		boolean[100001] notSelfNum = new boolean[100001];
+		
+		for (int i=1; i<= 10000; i++){
+			int b = i % 10;// 1ì˜ ìžë¦¬
+	       		int c = (i / 10) - d - e;//10ì˜ ìžë¦¬
+	        	int d = (i / 100) - e;//100ì˜ ìžë¦¬
+	        	int e = i / 1000;//1000ì˜ ìžë¦¬
+			
+			a = i + b + c + d + e;
+			
+			if ( a > 10001)
+				continue;
+			boolean[a] = true;
 		}
 		
-				
-		
+		for (int i=1; i<= 10000; i++){
+			if ( notSelfNum[i] != true)
+				System.out.println(i);
+		}
 	}
 }
